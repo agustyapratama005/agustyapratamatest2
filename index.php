f34b88223018dd829278e405edd1aed96914416a
agustyapratama005 <agustyapratama005@gmail.com>
2019-09-30 19:27:35 +0700

    menambahkan paragrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nihil pariatur cumque nostrum? Dolorum, corporis quod delectus dicta mollitia alias perferendis sint aspernatur adipisci soluta rerum voluptate, repellat incidunt voluptas?</p>
+</body>
+</html>
