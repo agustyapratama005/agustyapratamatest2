1640decd9eae0599eb562d36dd791c0faff572d5
agustyapratama005 <agustyapratama005@gmail.com>
2019-09-30 19:46:03 +0700

    menambahkan list 2

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nihil pariatur cumque nostrum? Dolorum, corporis quod delectus dicta mollitia alias perferendis sint aspernatur adipisci soluta rerum voluptate, repellat incidunt voluptas?</p>
+<ul>
+<li>satu</li>
+<li>dua</li>
+</ul>
 </body>
 </html>
